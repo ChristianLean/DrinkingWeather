0300a3e105f7d0b64f81c35ce07c4a563fab2b1e
Christian <christianlean31@gmail.com>
2017-02-04 22:07:12 +0800

    增加后台自动更新

---
+++ app/src/main/java/com/example/christianlee/drinkingweather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.example.christianlee.drinkingweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
